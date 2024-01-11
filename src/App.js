e034dca8fbd3e7e4eddf1b49a1c259928f390f95
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2024-01-10 19:25:54 -0500

    423 Reading and Dispatching from a new Slice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Fragment } from 'react';
+
 import Counter from './components/Counter';
 import Auth from './components/Auth.js';
 import Header from './components/Header.js';
+import UserProfile from './components/UserProfile.js';
 
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header/>
-      <Auth/>
+      { !isAuth && <Auth/> }
+      { isAuth && <UserProfile/> }
       <Counter />
     </Fragment>
   );
